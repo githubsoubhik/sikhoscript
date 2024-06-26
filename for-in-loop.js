8fdf57de161ba407f479a3c4c5d38f9d3c54258b
githubsoubhik <155788630+githubsoubhik@users.noreply.github.com>
2024-06-26 11:23:24 +0000

    for-in-loop

---
+++ for-in-loop.js
@@ -0,0 +1,14 @@
+//This lop is used for object
+
+
+const student={
+"name": "Soubhik",
+"marks": 25,
+"age": 20,
+"isPass": false
+}
+
+
+for(key in student){
+    console.log ("key=",key, "value=", student[key]);
+}
